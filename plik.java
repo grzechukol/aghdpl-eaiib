ddb5148801ace51589f597dc6af0f190f62887d2
Grzegorz Kolano <grzechukol@gmail.com>
2020-10-27 20:49:34 +0100

    Add listing examples

---
+++ plik.java
@@ -0,0 +1,29 @@
+/*
+java
+multiline
+comment
+*/
+public class SwapNumbers {
+
+    public static void main(String[] args) {
+
+        float first = 1.20f, second = 2.45f;
+
+        System.out.println("--Before swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second); // single line comment
+
+        // Value of first is assigned to temporary
+        float temporary = first;
+
+        // Value of second is assigned to first
+        first = second;
+
+        // Value of temporary (which contains the initial value of first) is assigned to second. This is supposed to be very long comment or line of the code to show that it can be wrapped by the LaTeX.
+        second = temporary;
+
+        System.out.println("--After swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
+    }
+}
